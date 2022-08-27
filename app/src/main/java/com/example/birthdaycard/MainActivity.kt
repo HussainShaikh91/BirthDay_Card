82c5b6565c2e0892ef5c8cbee2de6699a215f236
HussainShaikh91 <hussainshaikh3970@gmail.com>
2022-08-27 08:57:19 +0530

    Press back again to exit

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.birthdaycard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import com.example.birthdaycard.databinding.ActivityBirthdayBinding
 import com.example.birthdaycard.databinding.ActivityMainBinding
@@ -11,6 +13,8 @@ import com.example.birthdaycard.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,4 +35,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce){
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        //show Toast
+        Toast.makeText(this,"Please Press Back again to exit!",Toast.LENGTH_SHORT).show()
+        //decide time to click on back after showing toast
+        Handler(Looper.getMainLooper()).postDelayed(Runnable { doubleBackToExitPressedOnce = false },2000) //2000 millis is equal to 2 seconds
+
+    }
+
 }
